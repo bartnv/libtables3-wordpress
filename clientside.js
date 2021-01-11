12af2325e8174ffc2b2203efde53898ea997a907
Bart Noordervliet <bart@mmvi.nl>
2021-01-11 15:25:13 +0100

    Catch possible JS error

--- clientside.js
+++ clientside.js
@@ -382,7 +382,7 @@ function loadTable(div, attr, sub) {
     div.empty().append(tables[key].table);
     div.removeAttr('embedded');
   }
-  else if (tables[key] && tables[key].data.rowcount != -1) {
+  else if (tables[key] && tables[key].data && tables[key].data.rowcount != -1) {
     if (tables[key].doingajax) {
       console.log('Skipping load for', key, '(already in progress)');
       return;
